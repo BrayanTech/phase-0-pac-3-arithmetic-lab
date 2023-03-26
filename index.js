fef665d9bf7a1bb9baf4aeb67f33605e5362cde8
Brian Laureano <brayantechgeek@gmail.com>
2023-03-26 16:50:55 -0400

    updated

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+    return "something";
+}
+
+function subtract() {
+    return a - b;
+}
+
+function multiply() {
+    return a * b;
+}
+
+function divide() {
+    return a / b;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(a) {
+    return a + 1;
+}
+
+function decrement(a) {
+    return a - 1;
+}
+
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string, 10);
+}
